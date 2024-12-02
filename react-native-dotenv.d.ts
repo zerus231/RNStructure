552f48f209017ec8066581b5cbb1c258cf644d1d
LocNT <kusmile15@gmail.com>
2024-12-03 00:12:56 +0700

    add variable env

--- react-native-dotenv.d.ts
+++ react-native-dotenv.d.ts
@@ -1,3 +1,12 @@
 declare module 'react-native-dotenv' {
+  export const VITE_MAP_KEY: string;
+  export const VITE_FIREBASE_API_KEY: string;
+  export const VITE_FIREBASE_AUTH_DOMAIN: string;
+  export const VITE_FIREBASE_DATABASE_URL: string;
+  export const VITE_FIREBASE_PROJECT_ID: string;
+  export const VITE_FIREBASE_STORAGE_BUCKET: string;
+  export const VITE_FIREBASE_MESSAGING_SENDER_ID: string;
+  export const VITE_FIREBASE_APP_ID: string;
   export const VITE_API_BASE_URL: string;
+  export const VITE_GG_MAP_KEY: string;
 }
